aad35837f884566371c9b7072f0463885b44bbc1
Chimezie Enyinnaya <meziemichael@gmail.com>
2020-06-26 14:05:33 +0100

    feat: add mix helper

---
+++ providers/MixProvider.ts
@@ -0,0 +1,24 @@
+import { IocContract } from '@adonisjs/fold'
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+
+export default class MixProvider {
+  constructor (protected $container: IocContract) {}
+
+  public async boot () {
+    if (!this.$container.hasBinding('Adonis/Core/View')) {
+      throw new Error('You need to install the @adonisjs/view package to use this package.')
+    }
+
+    this.$container.with(['Adonis/Core/Application', 'Adonis/Core/View'], (Application: ApplicationContract, View) => {
+      View.global('mix', (assetPath: string) => {
+        if (!assetPath) {
+          return
+        }
+
+        const manifest = require(Application.publicPath('mix-manifest.json'))
+
+        return manifest[assetPath]
+      })
+    })
+  }
+}
